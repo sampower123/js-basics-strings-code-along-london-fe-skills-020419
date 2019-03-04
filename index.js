ee813fc5e13dc1a2b76a75b948c7c09bae7bde07
Sam Power <sam.g.power@gmail.com>
2019-03-04 20:20:43 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,4 +7,3 @@ const excitedWelcomeMessage = welcomeMessage.toUpperCase();
 
 const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`);
 
-const shortGreeting = `Welcome, ${currentUser.slice(0,1)}!`;
